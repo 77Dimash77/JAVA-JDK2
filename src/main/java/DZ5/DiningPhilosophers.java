5be33bd93e63fdcde3ac92fb0357936c2e4dc25c
Dimash <mwasikzano@gmail.com>
2023-12-17 13:17:29 +0600

    DZ5--

---
+++ src/main/java/DZ5/DiningPhilosophers.java
@@ -0,0 +1,50 @@
+package DZ5;
+
+import java.util.concurrent.Semaphore;
+
+class Philosopher extends Thread {
+    private static Semaphore semaphore = new Semaphore(2); // Допускаем 2 философа одновременно
+
+    private int id;
+    private int eatCount;
+
+    public Philosopher(int id) {
+        this.id = id;
+        this.eatCount = 0;
+    }
+
+    private void think() {
+        System.out.println("Philosopher " + id + " is thinking");
+    }
+
+    private void eat() throws InterruptedException {
+        semaphore.acquire(); // Пытаемся получить разрешение на обед
+        System.out.println("Philosopher " + id + " is eating (count: " + (eatCount + 1) + ")");
+        eatCount++;
+        Thread.sleep(500); // Время приема пищи
+        semaphore.release(); // Освобождаем разрешение после обеда
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (eatCount < 3) {
+                think();
+                eat();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class DiningPhilosophers {
+    public static void main(String[] args) {
+        Philosopher[] philosophers = new Philosopher[5];
+
+        for (int i = 0; i < 5; i++) {
+            philosophers[i] = new Philosopher(i);
+            philosophers[i].start();
+        }
+    }
+}
